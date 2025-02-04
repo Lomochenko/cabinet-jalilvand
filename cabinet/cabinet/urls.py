007aa6946743e7666b6e221e4f3aea1450260de2
moslem <hmtymslm50@gmail.com>
2025-02-04 17:13:10 +0330

    fix database

--- cabinet/cabinet/urls.py
+++ cabinet/cabinet/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_title = "داشبورد"
+admin.site.index_title = "dashboard"
+admin.site.site_header = "کابینت جلیلوند"
